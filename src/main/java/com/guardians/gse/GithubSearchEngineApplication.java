8f9f35168eb7d7ac691c38ae1c6e8073054634a1
Kiran Rajput <rajputkiran2805@gmail.com>
2025-03-20 23:48:03 +0530

    first commit

---
+++ src/main/java/com/guardians/gse/GithubSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.guardians.gse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guardians/gse/GithubSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guardians.gse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
