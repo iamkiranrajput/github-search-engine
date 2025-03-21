4785cd981d281d287d429466a67bcdcfffadd998
Kiran Rajput <rajputkiran2805@gmail.com>
2025-03-21 23:19:24 +0530

    DTO for search Request

--- src/main/java/com/guardians/gse/DTO/SearchRequest.java
+++ src/main/java/com/guardians/gse/DTO/SearchRequest.java
@@ -1,4 +1,14 @@
-package com.guardians.gse.DTO;
+package com.guardians.gse.dto;
 
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class SearchRequest {
+
+    @NotBlank(message = "Query cannot be empty")
+    private String query;
+    private String language;
+    private String sort;
 }
