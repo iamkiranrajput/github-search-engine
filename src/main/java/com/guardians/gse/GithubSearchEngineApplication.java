0a62ddb277b71046d9992ba186092d6acc29e61b
Kiran Rajput <rajputkiran2805@gmail.com>
2025-03-22 23:19:08 +0530

    Enabled redis cache

--- src/main/java/com/guardians/gse/GithubSearchEngineApplication.java
+++ src/main/java/com/guardians/gse/GithubSearchEngineApplication.java
@@ -2,8 +2,10 @@ package com.guardians.gse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GithubSearchEngineApplication {
 
 	public static void main(String[] args) {
