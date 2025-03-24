385da44d0895746844ea53911011aade63985f2f
Kiran Rajput <rajputkiran2805@gmail.com>
2025-03-24 23:40:06 +0530

    RepositoryRequest updated

--- src/main/java/com/guardians/gse/DTO/RepositoryRequest.java
+++ src/main/java/com/guardians/gse/DTO/RepositoryRequest.java
@@ -1,13 +1,20 @@
 package com.guardians.gse.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class RepositoryRequest {
+
+    @Schema(description = "Programming language filter", example = "Python")
     private String language;
+
+    @Schema(description = "Minimum stars required", example = "50")
     private Integer minStars;
+
+    @Schema(description = "Sorting order", example = "stars")
     private String sort;
 }
