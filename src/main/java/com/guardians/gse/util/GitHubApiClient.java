b1e6891f53b08ee2a30ddb0c1f8081accd4af0c5
Kiran Rajput <rajputkiran2805@gmail.com>
2025-03-24 23:27:49 +0530

    Util method Finding Repository by username

--- src/main/java/com/guardians/gse/util/GitHubApiClient.java
+++ src/main/java/com/guardians/gse/util/GitHubApiClient.java
@@ -4,29 +4,31 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.guardians.gse.controller.RepositoryController;
 import com.guardians.gse.dto.RepositoryDto;
 import com.guardians.gse.dto.SearchRequest;
-import com.guardians.gse.exception.GithubApiException;
+import com.guardians.gse.exception.GitHubRateLimitException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.client.HttpClientErrorException;
 
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static com.guardians.gse.mapper.Mapper.gitHubResponseToDto;
+
 @Component
 @Slf4j
 public class GitHubApiClient {
-    private static final String GITHUB_API_URL = "https://api.github.com/search/repositories";
+    private static final String GITHUB_PUBLIC_API_URL = "https://api.github.com/search/repositories";
+    private static final String GITHUB_USER_REPOS_URL = "https://api.github.com/users/{username}/repos";
+
     private final RestTemplate restTemplate;
 
     public GitHubApiClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    int RETRY = 3;
 
 
     public synchronized List<RepositoryDto> fetchRepositories(SearchRequest request) {
@@ -34,11 +36,11 @@ public class GitHubApiClient {
         List<RepositoryDto> repositories = new ArrayList<>();
         int page = 1;
         int perPage = 100;
-        int retryCount = 3; // Track retries locally
+        int RETRY = 3;
 
         while (true) {
             try {
-                String url = GITHUB_API_URL + "?q=" + query + "&sort=" + request.getSort() +
+                String url = GITHUB_PUBLIC_API_URL + "?q=" + query + "&sort=" + request.getSort() +
                         "&order=desc&per_page=" + perPage + "&page=" + page;
                 log.info("Fetching from GitHub API: {}", url);
 
@@ -47,25 +49,10 @@ public class GitHubApiClient {
 
                 if (items == null || items.isEmpty()) break;
 
-                for (JsonNode item : items) {
-                    RepositoryDto.Owner owner = new RepositoryDto.Owner(item.get("owner").get("login").asText());
+                for (JsonNode item : items) repositories.add(gitHubResponseToDto(item));
 
-                    RepositoryDto dto = new RepositoryDto(
-                            item.get("id").asInt(),
-                            item.get("name").asText(),
-                            item.has("full_name") ? item.get("full_name").asText() : "",
-                            item.has("description") ? item.get("description").asText() : "",
-                            item.get("stargazers_count").asInt(),
-                            item.get("forks_count").asInt(),
-                            item.has("html_url") ? item.get("html_url").asText() : "",
-                            item.has("language") ? item.get("language").asText() : "",
-                            Instant.parse(item.get("updated_at").asText()),
-                            owner
-                    );
-                    repositories.add(dto);
-                }
 
-                if (items.size() < perPage) break; // Stop if fewer results than perPage
+                if (items.size() < perPage) break;
                 page++;
             } catch (HttpClientErrorException.Forbidden e) {
                 if (page >= 10) {
@@ -76,11 +63,11 @@ public class GitHubApiClient {
 
                 log.error("Too many requests. Waiting 10 seconds before retrying...");
                 try {
-                    Thread.sleep(10000); // Wait before retrying
-                    retryCount--;
+                    Thread.sleep(10000);
+                    RETRY--;
 
-                    if (retryCount == 0) {
-                        throw new GithubApiException("GitHub API Rate Limit Exceeded: " + e.getMessage());
+                    if (RETRY == 0) {
+                        throw new GitHubRateLimitException("GitHub API Rate Limit Exceeded: " + e.getMessage());
                     }
 
                 } catch (InterruptedException ex) {
@@ -89,6 +76,42 @@ public class GitHubApiClient {
                 }
             }
         }
+        return repositories;
+    }
+
+
+
+
+
+
+
+
+
+
+    public List<RepositoryDto> fetchRepositoriesByUsername(String username) {
+        log.info("Fetching repositories for GitHub user: {}", username);
+        List<RepositoryDto> repositories = new ArrayList<>();
+
+        try {
+            ResponseEntity<JsonNode> response = restTemplate.getForEntity(GITHUB_USER_REPOS_URL, JsonNode.class, username);
+            JsonNode items = Objects.requireNonNull(response.getBody());
+
+            if (items.isEmpty()) {
+                log.info("No repositories found for user: {}", username);
+                return repositories;
+            }
+
+            for (JsonNode item : items) repositories.add(gitHubResponseToDto(item));
+
+        } catch (HttpClientErrorException.NotFound e) {
+            log.error("GitHub user not found: {}", username);
+            return null;
+        } catch (HttpClientErrorException.Forbidden e) {
+            log.error("GitHub API Rate Limit Exceeded: {}", e.getMessage());
+            return null;
+//            throw new GitHubRateLimitException("GitHub API Rate Limit Exceeded: " + e.getMessage());
+        }
+
         return repositories;
     }
 
