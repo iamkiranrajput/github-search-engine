d9c244d2a90d525ab7e60989b1286a5db9390b58
Kiran Rajput <rajputkiran2805@gmail.com>
2025-03-24 23:33:35 +0530

    repository service updated

--- src/main/java/com/guardians/gse/Service/RepositoryService.java
+++ src/main/java/com/guardians/gse/Service/RepositoryService.java
@@ -3,6 +3,7 @@ package com.guardians.gse.service;
 import com.guardians.gse.dto.RepositoryDto;
 import com.guardians.gse.dto.RepositoryRequest;
 import com.guardians.gse.dto.SearchRequest;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface RepositoryService {
 
     List<RepositoryDto> searchAndSaveRepositories(SearchRequest request, String query);
     List<RepositoryDto> getFilteredRepositories(RepositoryRequest request);
+    List<RepositoryDto> getRepositoriesByUsername(String username);
+    Page<RepositoryDto> getRepositoriesByUsernamePagination(String username, int page, int size, String sortBy, String direction);
 }
